0feb4b34534811b1495d8840a804e28187fb05b2
Spandan Saxena <spandansaxena@gmail.com>
2021-11-27 16:52:36 +0530

    Added Validation Checks on Fields

--- app/src/main/java/com/aaxena/ourjuet/Login.kt
+++ app/src/main/java/com/aaxena/ourjuet/Login.kt
@@ -3,8 +3,8 @@ package com.aaxena.ourjuet
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_login.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
@@ -13,79 +13,69 @@ import org.jsoup.nodes.Document
 import org.jsoup.select.Elements
 
 var captch = ""
-
+lateinit var mEnrollment: EditText
+lateinit var mPassword: EditText
+lateinit var mDob: EditText
 
 class Login: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        mEnrollment = findViewById(R.id.enrNumber)
+        mPassword = findViewById(R.id.password)
+        mDob = findViewById(R.id.dob)
     }
 
 
     fun connect(view: View) {
-        var loginform: Elements? = null
-        GlobalScope.launch(Dispatchers.IO) {
-
-            try {
-                val doc: Document = Jsoup.connect("https://webkiosk.juet.ac.in").get()
-                var id = doc.title()
-                Log.d("success", "${id.toString()}")
-                loginform = doc.getElementsByClass("noselect")
-                Log.d("sucess", "$loginform")
-                captch= loginform.toString().substring(32,37)
-                Log.d("sucess", "$loginform,${captch.length}")
-            } catch (e: Exception) {
-                Log.e("er", "$e=.toString())")
-            }}
-        captcha.text = captch
+        if (validate()){
+            var loginform: Elements? = null
+            GlobalScope.launch(Dispatchers.IO) {
+                try {
+                    val doc: Document = Jsoup.connect("https://webkiosk.juet.ac.in").get()
+                    var id = doc.title()
+                    Log.d("success", "$id")
+                    loginform = doc.getElementsByClass("noselect")
+                    Log.d("sucess", "$loginform")
+                    captch= loginform.toString().substring(32,37)
+                    Log.d("sucess", "$loginform,${captch.length}")
+                } catch (e: Exception) {
+                    Log.e("er", "$e=.toString())")
+                }}
+        }
     }
 }
 
-//    private fun init() {
-//        mEnrollment = findViewById<EditText>(R.id.enrNumber)
-//        mPassword = findViewById<EditText>(R.id.password)
-//        mDob = findViewById<EditText>(R.id.dob)
-//        mLogin = findViewById<Button>(R.id.loginButton)
-//        mLogin.setOnClickListener(this)
-//    }
+    fun validate(): Boolean {
+        if (mEnrollment.text.toString().isEmpty()) {
+            mEnrollment.error = "Enrollment Number is Required"
+           // vibrateDeviceError()
+            return false
+        } else if (mDob.text.toString().isEmpty()) {
+            mDob.error = "Date of Birth is Required"
+           // vibrateDeviceError()
+            return false
+        } else if (mPassword.text.toString().isEmpty()) {
+            mPassword.error = "Password is Required"
+           // vibrateDeviceError()
+            return false
+        }
+        return true
+    }
 
-//    fun validate(): Boolean {
-//        if (mEnrollment.getText().toString().isEmpty()) {
-//            mEnrollment.setError("Enrollment Number is Required")
-//            vibrateDeviceError()
-//            return false
-//        } else if (mDob.getText().toString().isEmpty()) {
-//            mDob.setError("Date of Birth is Required")
-//            vibrateDeviceError()
-//            return false
-//        } else if (mPassword.getText().toString().isEmpty()) {
-//            mPassword.setError("Password is Required")
-//            vibrateDeviceError()
-//            return false
-//        }
-//        return true
-//    }
+/*
+    private fun vibrateDeviceError() {
+        val v3 = this.getSystemService(VIBRATOR_SERVICE) as Vibrator
+        val pattern = longArrayOf(0, 25, 50, 35, 100)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            v3.vibrate(VibrationEffect.createWaveform(pattern, -1))
+        } else {
+            v3.vibrate(pattern, -1)
+        }
+    }
 
-//    private fun vibrateDeviceError() {
-//        val v3 = this.getSystemService(VIBRATOR_SERVICE) as Vibrator
-//        val pattern = longArrayOf(0, 25, 50, 35, 100)
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-//            v3.vibrate(VibrationEffect.createWaveform(pattern, -1))
-//        } else {
-//            v3.vibrate(pattern, -1)
-//        }
-//    }
-
-//    private fun vibrateDeviceSuccess() {
-//        val v3 = this.getSystemService(VIBRATOR_SERVICE) as Vibrator
-//        val pattern = longArrayOf(0, 25, 70, 38)
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-//            v3.vibrate(VibrationEffect.createWaveform(pattern, -1))
-//        } else {
-//            v3.vibrate(pattern, -1)
-//        }
-//    }
-//
+ */
+/*
 //    var dialog: ProgressDialog? = null
 //    fun showProgress() {
 //        dialog = ProgressDialog(this)
@@ -100,7 +90,4 @@ class Login: AppCompatActivity() {
 //    fun dismissProgress() {
 //        if (dialog != null && dialog!!.isShowing) dialog!!.dismiss()
 //    }
-//
-//    fun onClick(view: View?) {}
-
-
+ */
